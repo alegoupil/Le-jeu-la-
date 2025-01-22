ae162f072df334667dd06659d601b5732d6d2561
Antoine Legoupil <145803243+alegoupil@users.noreply.github.com>
2025-01-22 15:15:35 +0100

    Add files via upload

---
+++ Projet/source/lib/game.ml
@@ -0,0 +1,22 @@
+let game_hello () = print_endline "Hello, Newtonoiders!"
+
+(*
+Mise en place de quadtree (ne pas mettre de briques à cheval sur 2 quadtree)
+
+
+
+Fonctions:
+Position de la balle -> CalculNextPostion()
+- Collision briques
+- Colission Mur
+- Collisions Raquette (si la plateforme arrive vite sur la balle, augmentation de la vitesse horrizontale)
+Position de la raquette
+- Placer au même endroit que la souris
+- Calculer la vitesse ?
+
+
+Gravité
+Acceleration (plus la partie avance / briques cassées plus la balle "rebondi" fort)
+Score
+Vie
+*)
