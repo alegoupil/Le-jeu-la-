cd3c863c889ae7e23b8096d796b4a203ebd49e02
alexp <alexp.legall@gmail.com>
2025-01-26 17:24:27 +0100

    quadtree patch (à renommer) et briques patch

--- source/_build/default/lib/briques.ml
+++ source/_build/default/lib/briques.ml
@@ -1,9 +1,10 @@
 open Config
 open Quadtree
+
 (** type d'une brique : coordonnées de son coin inférieur gauche et sa couleur *)
 type br = (float * float) * Graphics.color
 (** collection contenant toutes les briques *)
-type t = br Quadtree.quadtree
+type t = br Quadtree.t
 
 (** nombre de briques en longueur et en hauteur : les briques sont agencées sur
     une grille de briques avec toutes les briques de la même taille *)
@@ -78,32 +79,60 @@ let%test "aucun_contact" =
   let vecteur_vitesse = (1., 1.) in
   contact_brique brique pos_balle vecteur_vitesse = (false, false)
 
-  let detecter_contact briques (position_balle : float * float) (vecteur_vitesse : float * float) = 
-    let pc_x, pc_y, _, _ = point_de_contact position_balle vecteur_vitesse in
-    let point_contact = fst (calculer_coordonnees_brique (pc_x, pc_y)) in
-    match Quadtree.get briques point_contact with
-    | None -> false, false
-    | Some brique -> contact_brique brique position_balle vecteur_vitesse 
-
+let detecter_contact briques (position_balle : float * float) (vecteur_vitesse : float * float) =
+  let pc_x, pc_y, _, _ = point_de_contact position_balle vecteur_vitesse in
+  let point_contact = fst (calculer_coordonnees_brique (pc_x, pc_y)) in
+  match Quadtree.get briques point_contact with
+  | None -> false, false
+  | Some brique -> contact_brique brique position_balle vecteur_vitesse
 
 let maj_briques arbre_briques position_balle vecteur_vitesse =
-  Quadtree.filtre_compter_retirer arbre_briques (fun brique ->
+  Quadtree.filter_val_count_removal arbre_briques (fun brique ->
     let contact_vertical, contact_horizontal = contact_brique brique position_balle vecteur_vitesse in
     not (contact_vertical || contact_horizontal))
 
-let inserer_brique arbre_briques brique =
-  let coordonnees_alignees, _ = calculer_coordonnees_brique (fst brique) in
-  Quadtree.insert arbre_briques coordonnees_alignees brique
+let inserer_brique : br Quadtree.t -> br -> br Quadtree.t =
+  fun br_qtree br ->
+    let coord, color = br in
+    (* Aligner les coordonnées avant d'insérer *)
+    let coord_aligned, _ = calculer_coordonnees_brique coord in
+    insert br_qtree coord_aligned (coord_aligned, color)
 
-let vide = Quadtree.vide ((0., 0.), (WindowConfig.width, WindowConfig.height))
+let%test "insertion_unique" =
+  let arbre_vide = Quadtree.empty ((0., 0.), (WindowConfig.width, WindowConfig.height)) in
+  let brique = ((50., 50.), Graphics.red) in
+  let coord_aligned, _ = calculer_coordonnees_brique (50., 50.) in
+  let arbre_avec_brique = inserer_brique arbre_vide brique in
+  Quadtree.get arbre_avec_brique coord_aligned = Some (coord_aligned, Graphics.red)
+
+let%test "insertion_multiple" =
+  let arbre_vide = Quadtree.empty ((0., 0.), (WindowConfig.width, WindowConfig.height)) in
+  let brique1 = ((50., 50.), Graphics.red) in
+  let brique2 = ((150., 50.), Graphics.blue) in
+  let coord1, _ = calculer_coordonnees_brique (50., 50.) in
+  let coord2, _ = calculer_coordonnees_brique (150., 50.) in
+  let arbre = inserer_brique (inserer_brique arbre_vide brique1) brique2 in
+  Quadtree.get arbre coord1 = Some (coord1, Graphics.red)
+  && Quadtree.get arbre coord2 = Some (coord2, Graphics.blue)
+
+let%test "insertion_duplicat" =
+  let arbre_vide = Quadtree.empty ((0., 0.), (WindowConfig.width, WindowConfig.height)) in
+  let brique1 = ((50., 50.), Graphics.red) in
+  let brique2 = ((50., 50.), Graphics.green) in
+  let coord_aligned, _ = calculer_coordonnees_brique (50., 50.) in
+  let arbre = inserer_brique (inserer_brique arbre_vide brique1) brique2 in
+  (* Vérifie que la dernière brique insérée remplace la première *)
+  Quadtree.get arbre coord_aligned = Some (coord_aligned, Graphics.green)
+
+
+let vide = Quadtree.empty ((0., 0.), (WindowConfig.width, WindowConfig.height))
 
 let collection_briques br_list =
   List.fold_left inserer_brique vide br_list
-
 
 let dessiner_brique ((x, y), couleur) =
   Graphics.set_color couleur;
   Graphics.fill_rect (int_of_float x) (int_of_float y) (int_of_float BrickConfig.width) (int_of_float BrickConfig.height)
 
 let dessiner_briques briques =
-  Quadtree.map briques dessiner_brique
+  Quadtree.iter_val briques dessiner_brique
--- source/lib/quadtree.ml
+++ source/lib/quadtree.ml
@@ -1,136 +1,154 @@
+(* coordonnées de l'objet inséré *)
 type coord = float * float
-type case = coord * coord
 
-type 'a quadtree =
-  | Vide of case
-  | Feuille of case * coord * 'a
-  | Noeud of case * 'a quadtree * 'a quadtree * 'a quadtree * 'a quadtree
+(* coordonnées des coins inférieur gauche et supérieur droit d'une case du quadtree *)
+type bound = coord * coord
 
-let centre_case ((x1, y1), (x2, y2)) = (x1 +. x2) /. 2., (y1 +. y2) /. 2.
+(* UTILS *)
 
-let%test _ = centre_case((0.,0.), (1.,1.)) = (0.5,0.5)
-let%test _ = centre_case((0.,0.), (0.,0.)) = (0.,0.)
-let nord_ouest : case -> case =
-  fun case ->
-  let (x1, y1), (_, _) = case in
-  let x2, y2 = centre_case case in
-  (x1, y1), (x2, y2)
+(** [center_from_bounds] calcule le centre d'une case du quadtree à partir de ses coordonnées
+    @param b les coordonnées des coins inférieur gauche et supérieur droit de la case
+    @return le centre de la case *)
+let center_from_bounds ((x1, y1), (x2, y2)) = (x1 +. x2) /. 2., (y1 +. y2) /. 2.
 
-let sud_ouest : case -> case =
-  fun case ->
-  let (_, y1), (x2, _) = case in
-  let x1, y2 = centre_case case in
-  (x1, y1), (x2, y2)
+(* fonctions qui calculent les coordonnées des coins des quatre sous-cases d'une case du quadtree *)
 
-let nord_est : case -> case =
-  fun case ->
-  let (x1, _), (_, y2) = case in
-  let x2, y1 = centre_case case in
-  (x1, y1), (x2, y2)
+let to_nw b =
+  let (x1, y1), (_, _) = b in
+  let cx, cy = center_from_bounds b in
+  (x1, y1), (cx, cy)
 
-let sud_est : case -> case =
-  fun case ->
-  let (_, _), (x2, y2) = case in
-  let x1, y1 = centre_case case in
-  (x1, y1), (x2, y2)
-  
-let%test _ = nord_ouest((1.,0.),(0.,1.)) = ((1.,0.),(0.5,0.5))
-let%test _ = nord_est((1.,0.),(0.,1.)) = ((1.,0.5),(0.5,1.))
-let%test _ = sud_ouest((1.,0.),(0.,1.)) = ((0.5,0.),(0.,0.5))
-let%test _ = sud_est((1.,0.),(0.,1.)) = ((0.5,0.5),(0.,1.))
+let to_ne b =
+  let (_, y1), (x2, _) = b in
+  let cx, cy = center_from_bounds b in
+  (cx, y1), (x2, cy)
 
-let vide case = Vide case
+let to_sw b =
+  let (x1, _), (_, y2) = b in
+  let cx, cy = center_from_bounds b in
+  (x1, cy), (cx, y2)
 
-let rec get : 'a quadtree -> coord -> 'a option =
-  fun tree (x, y) ->
-    match tree with 
-   | Vide _ ->  None
-   | Feuille (_, _, elem) -> Some(elem)
-   | Noeud (c,t1,t2,t3,t4) -> 
-    let x1, y1 = centre_case c in
-    if x > x1 && y > y1 then get t2 (x, y) else
-    if x > x1 && y < y1 then get t1 (x, y) else
-    if x < x1 && y < y1 then get t3 (x, y) 
-    else get t4 (x, y) 
+let to_se b =
+  let (_, _), (x2, y2) = b in
+  let cx, cy = center_from_bounds b in
+  (cx, cy), (x2, y2)
 
-let quadtree_test = Noeud (((1.,0.),(0.,1.)),Vide ((1.,0.),(0.5,0.5)),Feuille(((1.,0.5),(0.5,1.)),(0.75,0.75),1),Vide ((0.5,0.),(0.,0.5)),Vide ((0.5,0.5),(0.,1.)))
-let coord_test1 = (0.75,0.75)
-let coord_test2 = (0.,0.)
-let%test _ = get quadtree_test coord_test1 = Some(1)
-let%test _ = get quadtree_test coord_test2 = None
+(* QTREE *)
 
-let rec insert : 'a quadtree -> coord -> 'a -> 'a quadtree =
-  fun tree co elem ->
-  match tree with
-  | Vide case -> Feuille (case, co, elem)
-  | Feuille (case, co2, elem2) ->
-    if co = co2 then Feuille (case, co, elem)
+(** type du quadtree *)
+type 'a t =
+  | Empty of bound
+  | Leaf of bound * coord * 'a
+  | Node of bound * 'a t * 'a t * 'a t * 'a t
+
+(** [empty] crée un quadtree vide
+    @param b les coordonnées de la case initiale *)
+let empty b = Empty b
+
+(** [get] récupère la valeur associée à une coordonnée
+    @param t le quadtree
+    @param c la coordonnée
+    @return la valeur associée à la coordonnée *)
+let rec get : 'a t -> coord -> 'a option =
+  fun t (x, y) ->
+  match t with
+  | Empty _ -> None
+  | Leaf (_, _, v) -> Some v
+  | Node (b, q1, q2, q3, q4) ->
+    let cx, cy = center_from_bounds b in
+    if x < cx
+    then if y < cy then get q1 (x, y) else get q2 (x, y)
+    else if y < cy
+    then get q3 (x, y)
+    else get q4 (x, y)
+
+(** [insert] insère une valeur dans le quadtree
+    @param t le quadtree
+    @param c la coordonnée
+    @param v la valeur à insérer
+    @return le quadtree avec la valeur insérée *)
+let rec insert : 'a t -> coord -> 'a -> 'a t =
+  fun t c v ->
+  match t with
+  | Empty b -> Leaf (b, c, v)
+  | Leaf (b, oc, ov) ->
+    if c = oc
+    then Leaf (b, c, v)
     else (
-      let new_tree = Noeud (case, Vide (nord_ouest case), Vide (nord_est case), 
-                     Vide (sud_ouest case), Vide (sud_est case)) in
-      let new_tree = insert new_tree co2 elem2 in
-      insert new_tree co elem)
-  | Noeud (case, t1, t2, t3, t4) ->
-    let x, y = co in
-    let x1, y1 = centre_case case in
-    if x > x1 && y > y1 then Noeud (case, t1,insert t2 co elem, t3, t4) else
-    if x > x1 && y < y1 then Noeud (case, insert t1 co elem, t2, t3, t4) else
-    if x < x1 && y < y1 then Noeud (case, t1, t2, insert t3 co elem, t4) 
-    else Noeud (case, t1, t2, t3, insert t4 co elem)
+      let nt =
+        Node (b, Empty (to_nw b), Empty (to_sw b), Empty (to_ne b), Empty (to_se b))
+      in
+      let nt = insert nt oc ov in
+      insert nt c v)
+  | Node (b, q1, q2, q3, q4) ->
+    let x, y = c in
+    let cx, cy = center_from_bounds b in
+    (match x < cx, y < cy with
+     | true, true -> Node (b, insert q1 c v, q2, q3, q4)
+     | true, false -> Node (b, q1, insert q2 c v, q3, q4)
+     | false, true -> Node (b, q1, q2, insert q3 c v, q4)
+     | false, false -> Node (b, q1, q2, q3, insert q4 c v))
 
-let%test _ = insert quadtree_test coord_test2 2 = Noeud (((1.,0.),(0.,1.)),Vide ((1.,0.),(0.5,0.5)),Feuille(((1.,0.5),(0.5,1.)),(0.75,0.75),1),Feuille(((0.5,0.),(0.,0.5)),(0.,0.),2),Vide ((0.5,0.5),(0.,1.)))
-
-let clean_tree tree =
-  match tree with
-  | Vide _ -> tree
-  | Feuille _ -> tree
-  | Noeud (case, t1, t2, t3, t4) as n ->
-    (match t1, t2, t3, t4 with
-     | Vide _, Vide _, Vide _, Vide _ -> Vide case
-     | Feuille (_, co, elem), Vide _, Vide _, Vide _ -> Feuille (case, co, elem)
-     | Vide _, Feuille (_, co, elem), Vide _, Vide _ -> Feuille (case, co, elem)
-     | Vide _, Vide _, Feuille (_, co, elem), Vide _ -> Feuille (case, co, elem)
-     | Vide _, Vide _, Vide _, Feuille (_, co, elem) -> Feuille (case, co, elem)
+(** [prune_non_rec] supprime les sous-cases vides d'un noeud non récursivement
+    @param t le quadtree
+    @return le quadtree sans les sous-cases vides *)
+let prune_non_rec t =
+  match t with
+  | Empty _ -> t
+  | Leaf _ -> t
+  | Node (b, q1, q2, q3, q4) as n ->
+    (match q1, q2, q3, q4 with
+     | Empty _, Empty _, Empty _, Empty _ -> Empty b
+     | Leaf (_, c, v), Empty _, Empty _, Empty _ -> Leaf (b, c, v)
+     | Empty _, Leaf (_, c, v), Empty _, Empty _ -> Leaf (b, c, v)
+     | Empty _, Empty _, Leaf (_, c, v), Empty _ -> Leaf (b, c, v)
+     | Empty _, Empty _, Empty _, Leaf (_, c, v) -> Leaf (b, c, v)
      | _ -> n)
 
-let%test _ = clean_tree quadtree_test = Feuille(((1.,0.),(0.,1.)),(0.75,0.75),1)
+(** [remove] supprime un objet du quadtree en élaguant si nécessaire
+    @param t le quadtree
+    @param c la coordonnée
+    @return le quadtree sans l'objet *)
+let rec remove t (x, y) =
+  match t with
+  | Empty _ -> t
+  | Leaf (b, _, _) -> Empty b
+  | Node (b, q1, q2, q3, q4) ->
+    let cx, cy = center_from_bounds b in
+    (match x < cx, y < cy with
+     | true, true -> prune_non_rec (Node (b, remove q1 (x, y), q2, q3, q4))
+     | true, false -> prune_non_rec (Node (b, q1, remove q2 (x, y), q3, q4))
+     | false, true -> prune_non_rec (Node (b, q1, q2, remove q3 (x, y), q4))
+     | false, false -> prune_non_rec (Node (b, q1, q2, q3, remove q4 (x, y))))
 
-let rec retirer tree (x, y) =
-  match tree with
-  | Vide _ -> tree
-  | Feuille (case, _, _) -> Vide case
-  | Noeud (case, t1, t2, t3, t4) ->
-    let x1, y1 = centre_case case in
-    if x > x1 && y > y1 then clean_tree (Noeud (case, t1, retirer t2 (x, y), t3, t4)) else
-    if x > x1 && y < y1 then clean_tree (Noeud (case, retirer t1 (x, y), t2, t3, t4)) else
-    if x < x1 && y < y1 then clean_tree (Noeud (case, t1, t2, retirer t3 (x, y), t4))
-    else clean_tree (Noeud (case, t1, t2 , t3, retirer t4 (x, y)))
+(** [iter_val] applique une fonction à tous les objets du quadtree
+    @param t le quadtree
+    @param f la fonction à appliquer *)
+let rec iter_val t f =
+  match t with
+  | Empty _ -> ()
+  | Leaf (_, _, v) -> f v
+  | Node (_, q1, q2, q3, q4) ->
+    iter_val q1 f;
+    iter_val q2 f;
+    iter_val q3 f;
+    iter_val q4 f
 
-let%test _ = retirer quadtree_test (0.75,0.75) = Vide ((1.,0.),(0.,1.))
-
-let rec map tree f =
-  match tree with
-  | Vide _ -> ()
-  | Feuille (_, _, elem) -> f elem
-  | Noeud (_, t1, t2, t3, t4) ->
-    map t1 f;
-    map t2 f;
-    map t3 f;
-    map t4 f
-
-let filtre_compter_retirer tree f =
-  let rec aux tree count =
-    match tree with
-    | Vide _ -> tree, count
-    | Feuille (case, _, elem) -> if f elem then tree, count else Vide case, count + 1
-    | Noeud (case, t1, t2, t3, t4) ->
-      let t1, ncount = aux t1 count in
-      let t2, ncount = aux t2 ncount in
-      let t3, ncount = aux t3 ncount in
-      let t4, ncount = aux t4 ncount in
-      clean_tree (Noeud (case, t1, t2, t3, t4)), ncount
+(** [fold_val] filtre selon un prédicat et compte les objets enlevés
+    @param t le quadtree
+    @param f le prédicat
+    @return
+      le quadtree sans les objets qui ne vérifient pas le prédicat et le nombre d'objets enlevés *)
+let filter_val_count_removal t f =
+  let rec aux t acc =
+    match t with
+    | Empty _ -> t, acc
+    | Leaf (b, _, v) -> if f v then t, acc else Empty b, acc + 1
+    | Node (b, q1, q2, q3, q4) ->
+      let q1, nacc = aux q1 acc in
+      let q2, nacc = aux q2 nacc in
+      let q3, nacc = aux q3 nacc in
+      let q4, nacc = aux q4 nacc in
+      prune_non_rec (Node (b, q1, q2, q3, q4)), nacc
   in
-  aux tree 0
-
-let%test _ = filtre_compter_retirer quadtree_test (fun x -> x>0) = (Feuille(((1.,0.),(0.,1.)),(0.75,0.75),1), 0)
-let%test _ = filtre_compter_retirer quadtree_test (fun x -> x<0) = (Vide ((1.,0.),(0.,1.)), 1)
+  aux t 0
